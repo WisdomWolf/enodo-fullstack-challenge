974f416ac8ec6d54f2f03c5681f58b8198c65ff9
WisdomWolf <wisdomwolf@gmail.com>
2022-02-08 20:40:25 -0500

    Adds working startup script and readme

--- run_enodo_example_server.sh
+++ run_enodo_example_server.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+URL="http://localhost:8080"
+
 script_dir=`dirname "$0"`
 echo "initializing database"
 $script_dir/api/initialize-database.sh
 docker-compose up -d
-path=$(which open || which xdg-open || which gnome-open)
-exec "$path" "http://localhost:8080" || echo "Couldn't open a browser. Please visit http://localhost:8080 to view the site"
+path=$(which open 2>/dev/null || which xdg-open 2>/dev/null|| which gnome-open 2>/dev/null)
+exec "$path" "http://localhost:8080" && echo "browser should have opened to $URL" || echo "Couldn't open a browser. Please visit $URL to view the site"
