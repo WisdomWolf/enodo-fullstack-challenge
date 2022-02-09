9e77404c77c09b3c738974decb59c809632c7257
WisdomWolf <wisdomwolf@gmail.com>
2022-02-08 21:45:39 -0500

    Adds unit tests for api utils

---
+++ api/tests/test_backend.py
@@ -0,0 +1,35 @@
+from os import environ
+from pathlib import Path
+
+from openpyxl import load_workbook
+import pytest
+import yaml
+
+from utils.directory_resolvers import get_db_uri, get_file_path, get_project_path
+from utils.data_loader import resolve_headers
+
+
+@pytest.fixture
+def headers(shared_datadir):
+    file_path = Path(shared_datadir).joinpath('sample_data.yaml')
+    with open(file_path) as f:
+        data = yaml.safe_load(f)
+    return data['headers']
+
+
+def test_get_project_path():
+    module_path = Path(__file__)
+    assert module_path.is_relative_to(get_project_path())
+
+
+def test_get_db_uri():
+    environ['DATA_PATH'] = '/data'
+    assert get_db_uri('real_estate.db') == 'sqlite:////data/real_estate.db'
+
+
+def test_resolve_headers(headers):
+    excel_file_path = get_file_path('Enodo_Skills_Assessment_Data_File.xlsx')
+    wb = load_workbook(excel_file_path)
+    ws = wb.worksheets[0]
+    assert resolve_headers(ws) == headers
+
--- run_enodo_example_server.sh
+++ run_enodo_example_server.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
 URL="http://localhost:8080"
+if [ -n "$1" ];then
+  DOCKER_COMPOSE_ARGS="$1"
+else
+  DOCKER_COMPOSE_ARGS="--detach"
+fi
 
+echo "DOCKER_COMPOSE_ARGS: $DOCKER_COMPOSE_ARGS"
 script_dir=`dirname "$0"`
 echo "initializing database"
 $script_dir/api/initialize-database.sh
-docker-compose up -d
+docker-compose up $DOCKER_COMPOSE_ARGS
 path=$(which open 2>/dev/null || which xdg-open 2>/dev/null|| which gnome-open 2>/dev/null)
 exec "$path" "http://localhost:8080" && echo "browser should have opened to $URL" || echo "Couldn't open a browser. Please visit $URL to view the site"
